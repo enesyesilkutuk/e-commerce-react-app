98de3290b86f745590c2d47914b86e71d0a31c3d
enesyesilkutuk <enesyesilkutuk@gmail.com>
2023-02-14 13:42:47 +0300

    starter edited

---
+++ src/components/ProductList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import GridView from './GridView'
+import ListView from './ListView'
+
+const ProductList = () => {
+  return <h4>product list</h4>
+}
+
+export default ProductList
---
+++ src/pages/PrivateRoute.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+
+const PrivateRoute = () => {
+  return <h4>Private Route</h4>;
+};
+export default PrivateRoute;
